13b99d3a1c00e1807b5eed529140813bef65ba04
Ted Han <ted@knowtheory.net>
2019-05-28 14:57:07 -0700

    hax to calculate text positions

--- src/Notes.md
+++ src/Notes.md
@@ -19,6 +19,12 @@ PDFjs lifecycle notes:
 - There are a bunch of issues on github on destroying PDFs resulting in a memory leak.
   - adding an `onDestroy` method to the `DocumentRenderer` to clean up the `pdfjs` worker, and destroy the document is the way to go.
 
+### Text positioning
+
+So, it's possible to use `pdf.js`'s textLayer rendering system to generate bounding boxes for text elements.  Understanding how `pdf.js` does this is a bit complicated.  Each text object is represented using a vector of six numbers.  These six numbers describe the bounds, orientation, scale and skew for the element.
+
+
+
 
 [pdfjs-instructions]: https://github.com/mozilla/pdf.js/wiki/Setup-pdf.js-in-a-website
 
@@ -42,3 +48,53 @@ It's not clear that transforming the XML documents into a draft document is any 
 The other challenge is that only a subset of the documents in question have the legislative XML.
 
 Also see: https://github.com/usgpo/uslm/blob/master/USLM-User-Guide.md
+
+## Layout Analysis
+
+Okay, so as a general matter this topic is a huge can of worms.  There's a substantial body of literature about doing analysis of PDFs.  Additionally there are a pile of tools which claim to perform these sorts of analyses.  The best tool i've found in the past is pdf2htmlEX, which does an admirable job of matching the visual styles of a PDF in HTML + CSS.
+
+Text needs to be grouped.  Each text element needs to be assessed for its:
+
+- orientation
+- font characteristics:
+    - size
+    - font name / glyph
+
+Lines need to be grouped based on the similarity of text elements, and the dissimilarity to other elements.
+
+- whitespace: (consistency of column and margin space)
+- orientation of lines.
+
+### Component analyzers:
+
+- List identifiers
+- line number identifiers
+- Table of contents identifiers
+
+### pdf.js
+
+Okay, given the fact that we can inspect the output of the `pdf.js` text layer generator, we can reverse engineer its mechanisms for mapping text instructions in the PDF to positions on the canvas.
+
+# Annotated Bibliography
+
+### Breuel, Thomas M., "[High Performance Document Layout Analysis][breuel-2003a]", 2003 ([pdf link][breuel-2003a-pdf])
+
+[breuel-2003a]: https://www.semanticscholar.org/paper/High-Performance-Document-Layout-Analysis-Breuel/0acbc8706cb3e5a43141a03342ded6025a7eb60c
+[breuel-2003a-pdf]: https://pdfs.semanticscholar.org/0a78/09043fe4f50da53163d1fd318754c5f259e6.pdf
+
+This paper has some key observations despite being 16 years old.  It uses a variety of approaches composed into a single layout analysis system.
+
+#### Whitespace detection
+
+Breuel calculates a series of rectangles which maximize the area of whitespace each covers in the image.  Just finding all of the whitespace isn't super useful so they added some additional criteria which require whitespace rectangles to be bounded on some number of sides by other elements (either text or page boundary presumably).
+
+Breuel notes that in the simplest case calculating the whitespace is a matter of just maximizing the area in rectilinear areas orthogonal to the page bounds.  However there are a pile of cases where either complex layouts or poorly created documents may frustrate such an analysis.  He notes another paper in which he describes [a more robust mechanism for calculating whitespace boundaries independent of orientation][breuel-2003b].
+
+[breuel-2003b]: https://www.semanticscholar.org/paper/An-Algorithm-for-Finding-Maximal-Whitespace-at-for-Breuel/973d4afc87a911b2e6acb4a04059f2f5e85c66a9
+
+The orientation-free analysis essentially functions using four vertex parameters and an angle to indication the rotation of the box.
+
+### Konya, Iuliu Vasile and Stefan Eickeler. “[Logical structure recognition for heterogeneous periodical collections.][konya-eichkeler-2014]” DATeCH (2014).
+
+[konya-eichkeler-2014][https://www.semanticscholar.org/paper/Logical-structure-recognition-for-heterogeneous-Konya-Eickeler/861f520f6916f5badb96602717e27456c17e537d]
+
---
+++ src/pdfjs-hax.js
@@ -0,0 +1,119 @@
+let styleBuf = ['left: ', 0, 'px; top: ', 0, 'px; font-size: ', 0,
+'px; font-family: ', '', ';'];
+
+let calculateStyle = (item, styles, viewport, context) => {
+  // This is cribbed from the `appendText` function in text_layer.js
+  // START `appendText`
+  let textDiv = document.createElement('span');
+  let textDivProperties = {
+    style: null,
+    angle: 0,
+    canvasWidth: 0,
+    isWhitespace: false,
+    originalTransform: null,
+    paddingBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    scale: 1,
+  };
+
+  let tx = Util.transform(viewport.transform, item.transform);
+  // 
+  let angle = Math.atan2(tx[1], tx[0]);
+  let style = styles[item.fontName];
+  if (style.vertical) {
+    angle += Math.PI / 2;
+  }
+  // i'm still not clear what tx[2] and tx[3] represent.
+  let fontHeight = Math.sqrt((tx[2] * tx[2]) + (tx[3] * tx[3]));
+  // assume the that the font's ascenders are equal to the font height.
+  let fontAscent = fontHeight;
+  if (style.ascent) {
+    // if the ascent is specified in the styles, scale the fontAscent
+    // by the style's ascent
+    fontAscent = style.ascent * fontAscent;
+  } else if (style.descent) {
+    // otherwise if the descent is specified modify by that.
+    fontAscent = (1 + style.descent) * fontAscent;
+  }
+
+  let left;
+  let top;
+  if (angle === 0) {
+    // if the text isn't rotated...
+    left = tx[4];               // the left bound is tx[4]
+    top = tx[5] - fontAscent;   // the top bound is tx[5] discounted by the fontAscent
+  } else {
+    // tx[4] plus the height of the element modified by the angle.
+    // so for example, if angle is 90°, it's sine is 1.
+    // Thus left is tx[4] + fontAscent
+    // if angle is 45°, left is `tx[4] + (fontAscent * 0.5)`
+    left = tx[4] + (fontAscent * Math.sin(angle));
+    // Likewise if angle is 90° top is `tx[5] - (fontAscent * 0)`
+    // and if angle is 0° top is `tx[5] - (fontAscent * 1)`
+    // and if angle is 45° top is `tx[5] - (fontAscent * 0.5)`
+    top = tx[5] - (fontAscent * Math.cos(angle));
+    // So if we think about this basically, when a rotation happens with positive THETA
+    // the element is rotated around its center, and the LEFT edge of the boundary increases 
+    // by some fraction of the height+ascent of the text element.
+    //
+    // The BOTTOM LEFT corner must be moved further to the right
+    // such that the TOP LEFT corner sits at tx[4] as it swings back to the left.
+    // 
+    // Likewise, as the TOP RIGHT corner swings UPWARD, the `top` field must be diminished
+    // by the appropriately rotated fraction of the fontAscent so that the TOP RIGHT corner
+    // sits at tx[5].
+  }
+  styleBuf[1] = left;
+  styleBuf[3] = top;
+  styleBuf[5] = fontHeight;
+  styleBuf[7] = style.fontFamily;
+  textDivProperties.style = styleBuf.join('');
+  textDiv.setAttribute('style', textDivProperties.style);
+
+  textDiv.textContent = item.str;
+  // `fontName` is only used by the FontInspector, and we only use `dataset`
+  // here to make the font name available in the debugger.
+  /*if (task._fontInspectorEnabled) {
+    textDiv.dataset.fontName = item.fontName;
+  }*/
+  if (angle !== 0) {
+    textDivProperties.angle = angle * (180 / Math.PI);
+  }
+  if (style.vertical) {
+    textDivProperties.canvasWidth = item.height * viewport.scale;
+  } else {
+    textDivProperties.canvasWidth = item.width * viewport.scale;
+  }
+  // END `appendText`
+  // START `_layoutText`
+
+  let fontSize = textDiv.style.fontSize;
+  let fontFamily = textDiv.style.fontFamily;
+
+  // pdf.js batches this and only set the font
+  // if it differs between two text elements
+  context.font = `${fontSize} ${fontFamily}`;
+  let width = context.measureText(textDiv.textContent).width;
+  let transform = '';
+
+  if (textDivProperties.canvasWidth !== 0 && width > 0) {
+    //// the scale is equal to the proportion of the `canvasWidth` to the calculated `width`
+    textDivProperties.scale = textDivProperties.canvasWidth / width;
+    transform = `scaleX(${textDivProperties.scale})`;
+  }
+  //// rotate the text as needed.
+  if (textDivProperties.angle !== 0) {
+    transform = `rotate(${textDivProperties.angle}deg) ${transform}`;
+  }
+  //// set the transform into the styles if there are any.
+  if (transform.length > 0) {
+    textDivProperties.originalTransform = transform;
+    textDiv.style.transform = transform;
+  }
+
+  return textDivProperties;
+};
+
+export default { calculateStyle };
