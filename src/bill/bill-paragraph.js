486ee3d28044911c56469f83ea1e85c43b041183
Ted Han <ted@knowtheory.net>
2019-06-24 08:02:56 -0700

    Shift all margins to single initial lines.

--- src/bill/bill-paragraph.js
+++ src/bill/bill-paragraph.js
@@ -67,9 +67,10 @@ class BillParagraph {
     if (this.pageBreak) { graf = graf.pageBreak(); }
     if (this.styles.margin && this.styles.margin > 0) {
       let twips = Utils.pixelsToTWIPs(this.styles.margin);
-      graf = graf.indent({left: twips});
+      graf = graf.indent({firstLine: twips,});
     }
-    doc.addParagraph(graf.spacing({line: 240*2}));
+    graf = graf.spacing({before: 240});
+    doc.addParagraph(graf);
   }
 }
 
