1ef78b68f38bcdc025d882da248a99687827d4d0
Ted Han <ted@knowtheory.net>
2019-09-05 23:53:52 -0700

    Google analytics!

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import App from './App.svelte';
+import googleAnalytics from './google-analytics.js';
+
+const env = JSON.parse('ENVIRONMENT_DATA')
+if (env['ga_id']) {
+	//console.log("Running in production!");
+	googleAnalytics(env['ga_id']);
+} else {
+	//console.log("Not running in production!");
+}
 
 const app = new App({
 	target: document.body,
